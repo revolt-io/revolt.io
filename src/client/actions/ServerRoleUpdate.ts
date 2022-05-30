3fd735385280313964aa9f6ac3458b72843f2849
abdulrahman1s <61483023+abdulrahman1s@users.noreply.github.com>
2022-05-30 20:57:37 +0200

    feat: Emit role updates

---
+++ src/client/actions/ServerRoleUpdate.ts
@@ -0,0 +1,18 @@
+import { API } from '../../../deps.ts'
+import { Action } from './Action.ts'
+import { Events } from '../../util/Constants.ts'
+
+export class ServerRoleUpdateAction extends Action {
+  handle(data: { id: string, role_id: string, data: API.Role, clear: API.FieldsRole[] }): void {
+    const server = this.client.servers.cache.get(data.id)
+
+    if (!server) return
+
+    const role = server.roles.cache.get(data.role_id)
+    const oldRole = role?._update(data.data, data.clear)
+
+    if (role && oldRole && !role.equals(oldRole)) {
+      this.client.emit(Events.ROLE_UPDATE, oldRole, role)
+    }
+  }
+}
