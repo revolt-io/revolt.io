3f508e6cb95964ee2b3d3e6f3f1be246928b7a5b
abdulrahman1s <abdulrahman.8alah@gmail.com>
2022-05-30 21:01:45 +0200

    feat: Add Presence class

---
+++ src/structures/Presence.ts
@@ -0,0 +1,13 @@
+import { Base } from './Base.ts'
+
+export enum Status {
+  ONLINE = 'ONLINE',
+  IDLE = 'IDLE',
+  BUSY = 'DND',
+  INVISIBLE = 'OFFLINE',
+}
+
+export class Presence extends Base {
+  text: string | null = null
+  status = Status.INVISIBLE
+}
