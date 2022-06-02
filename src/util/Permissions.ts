b4399a3ad19835a28b91396ac97f7da5336d5b0d
abdulrahman1s <abdulrahman.8alah@gmail.com>
2022-06-03 01:47:23 +0200

    fix: Expose bitfields flags

--- src/util/Permissions.ts
+++ src/util/Permissions.ts
@@ -1,8 +1,8 @@
 import { BitField } from './BitField.ts';
 
-export type ChannelPermissionsString = keyof typeof FLAGS['CHANNEL'];
-export type UserPermissionsString = keyof typeof FLAGS['USER'];
-export type ServerPermissionsString = keyof typeof FLAGS['SERVER'];
+export type ChannelPermissionsString = keyof typeof ChannelPermissions.FLAGS;
+export type UserPermissionsString = keyof typeof UserPermissions.FLAGS;
+export type ServerPermissionsString = keyof typeof ServerPermissions.FLAGS;
 export type ChannelPermissionsResolvable =
   | number
   | ChannelPermissionsString
@@ -43,8 +43,8 @@ export declare interface UserPermissions {
   has(bit: UserPermissionsResolvable): boolean;
 }
 
-const FLAGS = {
-  CHANNEL: {
+export class ChannelPermissions extends BitField {
+  static readonly FLAGS = {
     VIEW_CHANNEL: 1 << 0,
     SEND_MESSAGE: 1 << 1,
     MANAGE_MESSAGE: 1 << 2,
@@ -53,14 +53,36 @@ const FLAGS = {
     INVITE_OTHERS: 1 << 5,
     EMBED_LINKS: 1 << 6,
     UPLOAD_FILES: 1 << 7,
-  },
-  USER: {
+  } as const;
+
+  constructor(bits?: ChannelPermissionsResolvable) {
+    super(bits);
+  }
+  
+  static resolve(bit: ChannelPermissionsResolvable): number {
+    return super.resolve(bit);
+  }
+}
+
+export class UserPermissions extends BitField {
+  static readonly FLAGS = {
     ACCESS: 1 << 0,
     VIEW_PROFILE: 1 << 1,
     SEND_MESSAGES: 1 << 2,
     INVITE: 1 << 3,
-  },
-  SERVER: {
+  } as const;
+
+  constructor(bits?: UserPermissionsResolvable) {
+    super(bits);
+  }
+
+  static resolve(bit: UserPermissionsResolvable): number {
+    return super.resolve(bit);
+  }
+}
+
+export class ServerPermissions extends BitField {
+  static readonly FLAGS = {
     VIEW_SERVER: 1 << 0,
     MANAGE_ROLES: 1 << 1,
     MANAGE_CHANNELS: 1 << 2,
@@ -71,44 +93,16 @@ const FLAGS = {
     MANAGE_NICKNAMES: 1 << 13,
     CHANGE_AVATAR: 1 << 14,
     REMOVE_AVATARS: 1 << 15,
-  },
-} as const;
+  } as const;
 
-export class ChannelPermissions extends BitField {
-  static FLAGS: typeof FLAGS['CHANNEL'];
-  constructor(bits?: ChannelPermissionsResolvable) {
-    super(bits);
-  }
-  static resolve(bit: ChannelPermissionsResolvable): number {
-    return super.resolve(bit);
-  }
-}
-
-ChannelPermissions.FLAGS = FLAGS.CHANNEL;
-
-export class UserPermissions extends BitField {
-  static FLAGS: typeof FLAGS['USER'];
-  constructor(bits?: UserPermissionsResolvable) {
-    super(bits);
-  }
-  static resolve(bit: UserPermissionsResolvable): number {
-    return super.resolve(bit);
-  }
-}
-
-UserPermissions.FLAGS = FLAGS.USER;
-
-export class ServerPermissions extends BitField {
-  static FLAGS: typeof FLAGS['SERVER'];
   constructor(bits?: ServerPermissionsResolvable) {
     super(bits);
   }
+
   static resolve(bit: ServerPermissionsResolvable): number {
     return super.resolve(bit);
   }
 }
-
-ServerPermissions.FLAGS = FLAGS.SERVER;
 
 export const DEFAULT_PERMISSION_DM = new ChannelPermissions([
   'VIEW_CHANNEL',
